5e1edbb54bf946e799f15933788b32a532777c08
Eurico Fonseca <eurico5@sapo.pt>
2016-09-29 09:31:27 +0100

    defined the size of the array in Threes and Fives exercise

--- semana 2/ThreesandFives.java
+++ semana 2/ThreesandFives.java
@@ -4,9 +4,14 @@ class ThreesandFives{
     if(args.length>0){
       value = Integer.parseInt(args[0]);
     }
-    
-    int[] threesOrFives = new int[value];
-    int[] thressAndFives = new int[value];
+
+    int size1 = (int)((value * 6)/15) + 2;
+    int size2 = (int)(value / 15);
+
+    //System.out.println(size1 + " " + size2);
+
+    int[] threesOrFives = new int[size1];
+    int[] thressAndFives = new int[size2];
     int i = 0;
     int j = 0;
 
@@ -21,15 +26,13 @@ class ThreesandFives{
       }
       value--;
     }
-
+    System.out.println("Thress and fives");
     for(int a = 0; a < thressAndFives.length; a++){
-      if(thressAndFives[a] != 0 ){
-        System.out.println(thressAndFives[a]);
-      }
-
+      System.out.println(thressAndFives[a]);
     }
+    System.out.println("Thress or fives");
     for(int a = 0; a < threesOrFives.length; a++){
-      if(threesOrFives[a] != 0){
+      if(threesOrFives[a] != 1){
         System.out.println(threesOrFives[a]);
       }
     }
